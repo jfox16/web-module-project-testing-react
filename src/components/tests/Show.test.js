e8e9978c0cae5119ec1505b3304dd91d73663aa9
Jonathan Fox <FoxJonathanP@gmail.com>
2021-03-23 19:36:12 -0700

    finished all test

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -5,22 +5,55 @@ import userEvent from '@testing-library/user-event';
 import Show from './../Show';
 
 const testShow = {
-    //add in approprate test data structure here.
+    name: 'test show',
+    summary: 'test summary',
+    seasons: [
+        {
+            id: 0,
+            name: 'Season 1',
+            episodes: []
+        },
+        {
+            id: 1,
+            name: 'Season 2',
+            episodes: []
+        }
+    ]
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
+    render(<Show show={testShow} selectedSeason={0} />);
 });
 
 test('renders Loading component when prop show is null', () => {
+    render(<Show selectedSeason={0} />);
+    const loader = screen.getByTestId('loading-container');
+    expect(loader).toBeInTheDocument();
 });
 
 test('renders same number of options seasons are passed in', ()=>{
+    render(<Show show={testShow} selectedSeason={0} />);
+    const seasonOptions = screen.getAllByTestId('season-option');
+    expect(seasonOptions.length).toEqual(testShow.seasons.length);
 });
 
 test('handleSelect is called when an season is selected', () => {
+    const handleSelect = jest.fn();
+    render(<Show show={testShow} selectedSeason={'none'} handleSelect={handleSelect} />);
+    const select = screen.getByLabelText(/select a season/i);
+    userEvent.selectOptions(select, ['1']);
+
+    expect(handleSelect).toBeCalled();
 });
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    const { rerender } = render(<Show show={testShow} selectedSeason={'none'} />);
+    let episodes = screen.queryByTestId('episodes-container');
+    expect(episodes).not.toBeInTheDocument();
+
+    rerender(<Show show={testShow} selectedSeason={1} />);
+    episodes = screen.queryByTestId('episodes-container');
+    expect(episodes).toBeInTheDocument();
 });
 
 //Tasks:
